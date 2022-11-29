0ee0924eadbb3a442389e996083ef9bfabdec26d
Anajuliarlc <anajuliaritalc@gmail.com>
2022-11-28 23:39:56 -0300

    Faça os botoes

--- codigo/menu.py
+++ codigo/menu.py
@@ -1,8 +1,11 @@
 import pygame as pg
 from tela import Tela
+from botao import Botao
+
 
 class TelaMenu(Tela):
     """Classe que organiza o menu do jogo"""
+
     def __init__(self, titulo, icone, imagem_fundo):
         """ Cria a tela do menu
 
@@ -12,81 +15,73 @@ class TelaMenu(Tela):
         :type icone: str
         :param imagem_fundo: Caminho de acesso da imagem de fundo
         :type imagem_fundo: str
-        """             
+        """
         super().__init__(titulo, icone, imagem_fundo)
 
     def iniciar(self):
-        """Inicia o menu do jogo"""
         screen = pg.display.set_mode((self.largura, self.altura))
 
-        #Titulo da janela
+        # Titulo da janela
         pg.display.set_caption(self.titulo)
 
-        #Coloca o icone na tela
+        # Coloca o icone na tela
         icone = pg.image.load(self.icone)
         pg.display.set_icon(icone)
 
-        #Tempo de atualização da tela
+        # Tempo de atualização da tela
         clock = pg.time.Clock()
+
+        botao_jogar = Botao(100, 100, 100, 50, (255, 255, 255),
+                            "Jogar", (0, 0, 0), "italic", 20)
+
+        botao_sair = Botao(100, 300, 100, 50, (255, 255, 255),
+                           "Sair", (0, 0, 0), "italic", 20)
+
+        dicionario_botoes = {0: botao_jogar, 1: botao_sair}
+
+        botao_selecionado = 0
 
         # Loop principal
         running = True
         while running:
+
+            # Cor de fundo
+            screen.fill((202, 30, 50))
+            dicionario_botoes[botao_selecionado].definir_cor_fundo(
+                (77, 77, 77))
+            botao_jogar.desenhar_botao(screen)
+
+            botao_sair.desenhar_botao(screen)
+
             pg.event.pump()
             keys = pg.key.get_pressed()
 
             teclas = dict()
-            teclas[0] = keys[pg.K_o]
+            teclas[0] = keys[pg.K_w]
+            teclas[1] = keys[pg.K_s]
+            teclas[2] = keys[pg.K_KP_ENTER]
 
-            if teclas[0]:
-                return "Configuracoes"
+            if teclas[0] == True and botao_selecionado > 0:
+                dicionario_botoes[botao_selecionado].definir_cor_fundo(
+                    (255, 255, 255))
+                botao_selecionado -= 1
+
+            elif teclas[1] == True and botao_selecionado < 1:
+                dicionario_botoes[botao_selecionado].definir_cor_fundo(
+                    (255, 255, 255))
+                botao_selecionado += 1
+
+            elif teclas[2] == True:
+                pass
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     return "Sair"
 
-            #Cor de fundo
-            screen.fill((54, 98, 86))
             pg.display.update()
 
             clock.tick(self.fps)
 
-tela = TelaMenu("Menu", "niveis/tilesets/personagem_provisorio.png", "niveis/tilesets/personagem_provisorio.png")
-tela.iniciar()
 
-class Botao():
-    def __init__(self, x, y, largura, altura, cor, texto, fonte, cor_texto):
-        self.x = x
-        self.y = y
-        self.largura = largura
-        self.altura = altura
-        self.cor = cor
-        self.texto = texto
-        self.fonte = fonte
-        self.cor_texto = cor_texto
-
-    def desenhar(self, tela):
-        pg.draw.rect(tela, self.cor, (self.x, self.y, self.largura, self.altura))
-        texto = self.fonte.render(self.texto, True, self.cor_texto)
-        tela.blit(texto, (self.x + (self.largura/2 - texto.get_width()/2), self.y + (self.altura/2 - texto.get_height()/2)))
-
-    def abrir(self, tela):
-        running = True
-        while running:
-            pg.event.pump()
-            keys = pg.key.get_pressed()
-
-            teclas = dict()
-            teclas[0] = keys[pg.K_s]
-            teclas[1] = keys[pg.K_w]
-            teclas[2] = keys[pg.K_SPACE]
-
-            if teclas[0]:
-                return "Configuracoes"
-            
-            elif teclas[1]:
-                return "Inventario"
-
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    return "Sair"
+TelaMenu("Menu", "niveis/tilesets/anya_provisorio_c.png",
+         "niveis/tilesets/anya_provisorio_c.png").iniciar()
