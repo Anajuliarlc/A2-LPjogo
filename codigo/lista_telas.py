b1cd1980afc33e3610643b2c367ce3acaa75169a
Anajuliarlc <anajuliaritalc@gmail.com>
2022-12-01 19:58:46 -0300

    Integre as telas e o enumerate

--- codigo/lista_telas.py
+++ codigo/lista_telas.py
@@ -1,9 +1,11 @@
 from tela_nivel import TelaNivel
+from menu import TelaMenu
 from enum import Enum
+
 
 class ListaTelas(Enum):
     """Enumeração de telas do jogo"""
-    tela_inicial = TelaNivel("Tela_inicial", "niveis/personagem/personagem_provisorio.png")
+    tela_inicial = TelaMenu("Menu", "niveis/personagem/anya_provisorio_c.png")
     tela_opcoes = TelaNivel("Opções", "niveis/personagem/personagem_provisorio.png")
     tela_inventario = TelaNivel("Inventario", "niveis/personagem/personagem_provisorio.png")
-    tela_nivel= TelaNivel("Nivel 1", "niveis/personagem/personagem_provisorio.png")
+    tela_nivel = TelaNivel("Nivel 1", "niveis/personagem/personagem_provisorio.png")
