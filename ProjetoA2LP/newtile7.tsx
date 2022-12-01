acfed471070ce1187b589f78e0c9aafa2d09e035
LuisHDBueno <105614892+LuisHDBueno@users.noreply.github.com>
2022-12-01 12:58:53 -0300

    Organize as pastas do nível

--- ProjetoA2LP/newtile7.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="newtile7" tilewidth="32" tileheight="32" tilecount="8304" columns="8">
- <image source="níveis/tiled_set/images/newtile7.PNG" width="256" height="33216"/>
-</tileset>
--- ProjetoA2LP/níveis/newtile7 - Copia.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="newtile7 - Copia" tilewidth="32" tileheight="32" tilecount="8304" columns="1038">
- <image source="tiled_set/images/newtile7 - Copia.PNG" width="33216" height="256"/>
-</tileset>
