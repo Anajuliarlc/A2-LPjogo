9d5ec2fe72c69a618573f0870830ad34807f2dbc
Anajuliarlc <anajuliaritalc@gmail.com>
2022-12-04 13:59:23 -0300

    resolva

--- codigo/tela_inventario.py
+++ codigo/tela_inventario.py
@@ -4,38 +4,38 @@ from botao import Botao
 from lista_controles import ListaControles
 from lista_retornos import ListaRetornos
 
+
 class TelaInventario(Tela):
     """Tela do Inventário do jogo """
 
-    def __init__(self, titulo, icone): 
-         """ Cria a tela do inventário
-        :param titulo: Título da tela        
-        :type titulo: str
-        :param icone: Caminho de acesso do ícone da tela em png
-        :type icone: str
-        """
-         super().__init__(titulo, icone) 
-
+    def __init__(self, titulo, icone):
+        """ Cria a tela do inventário
+       :param titulo: Título da tela        
+       :type titulo: str
+       :param icone: Caminho de acesso do ícone da tela em png
+       :type icone: str
+       """
+        super().__init__(titulo, icone)
 
     def controles(self):
         """Verifica os inputs do usuário e define os controles dessa tela
         :return: Retorna um dicionário com o nome do controle como chave e True ou False como valor
         :rtype: dict
-        """ 
-        #Verficia os inputs do usuário
+        """
+        # Verficia os inputs do usuário
         pg.event.pump()
         pressionados = pg.key.get_pressed()
 
-        #Define os controles dessa tela
+        # Define os controles dessa tela
         teclas = dict()
-    
+
         teclas[ListaControles.cima.name] = pressionados[ListaControles.cima.value]
         teclas[ListaControles.baixo.name] = pressionados[ListaControles.baixo.value]
         teclas[ListaControles.enter.name] = pressionados[ListaControles.enter.value]
 
         return teclas
 
-def iniciar(self):
+    def iniciar(self):
         screen = pg.display.set_mode((self.largura, self.altura))
 
         # Titulo da janela
@@ -48,70 +48,79 @@ def iniciar(self):
         # Tempo de atualização da tela
         clock = pg.time.Clock()
 
-        # Definindo cada imagem 
+        # Definindo cada imagem
 
-        doll = pg.image.load("niveis/imagens_tilestes/doll.png").convert_alpha()
-        doll_scale = pg.transform.scale(doll_scale, (100, 100))
+        doll = pg.image.load(
+            "niveis/level_data/imagens_tilsetes/doll.png").convert_alpha()
+        doll_scale = pg.transform.scale(doll, (100, 100))
 
-        green_book = pg.image.load("niveis/imagens_tilestes/green-book.png").convert_alpha()    
-        green_book_scale = pg.transform.scale(green_book, (100, 100))    
+        green_book = pg.image.load(
+            "niveis/level_data/imagens_tilsetes/green-book.png").convert_alpha()
+        green_book_scale = pg.transform.scale(green_book, (100, 100))
 
-        heart_scale = pg.image.load("niveis/imagens_tilestes/heart-scale.png").convert_alpha()
+        heart_scale = pg.image.load(
+            "niveis/level_data/imagens_tilsetes/heart-scale.png").convert_alpha()
         heart_scale = pg.transform.scale(heart_scale, (100, 100))
 
-        letter = pg.image.load("niveis/imagens_tilestes/letter.png").convert_alpha()
+        letter = pg.image.load(
+            "niveis/level_data/imagens_tilsetes/letter.png").convert_alpha()
         letter_scale = pg.transform.scale(letter, (100, 100))
-        
-        pearl = pg.image.load("niveis/imagens_tilestes/").convert_alpha()
+
+        pearl = pg.image.load(
+            "niveis/level_data/imagens_tilsetes/pearl.png").convert_alpha()
         pearl_scale = pg.transform.scale(pearl, (100, 100))
 
-        pink_heart = pg.image.load("niveis/imagens_tilestes/pink-heart.png").convert_alpha()
+        pink_heart = pg.image.load(
+            "niveis/level_data/imagens_tilsetes/pink-heart.png").convert_alpha()
         pink_heart_scale = pg.transform.scale(pink_heart, (100, 100))
 
-        teapot = pg.image.load("niveis/imagens_tilestes/teapot.png").convert_alpha()
+        teapot = pg.image.load(
+            "niveis/level_data/imagens_tilsetes/teapot.png").convert_alpha()
         teapot_scale = pg.transform.scale(teapot, (100, 100))
 
-        ticket = pg.image.load("niveis/imagens_tilestes/ticket.png").convert_alpha()          
+        ticket = pg.image.load(
+            "niveis/level_data/imagens_tilsetes/ticket.png").convert_alpha()
         ticket_scale = pg.transform.scale(ticket, (100, 100))
 
-        wreathe_of_flowers = pg.image.load("niveis/imagens_tilestes/wreathe-of-flowers.png").convert_alpha()  
-        wreathe_of_flowers_scale = pg.transform.scale(wreathe_of_flowers, (100, 100))
+        wreathe_of_flowers = pg.image.load(
+            "niveis/level_data/imagens_tilsetes/wreathe-of-flowers.png").convert_alpha()
+        wreathe_of_flowers_scale = pg.transform.scale(
+            wreathe_of_flowers, (100, 100))
 
         "pos_x, pos_y, largura, altura, cor_fundo, texto, cor_texto, fonte, tamanho_fonte"
-        botao_doll = Botao(430, 250, 200, 80, (239, 216, 237),
-                            "Doll", (0, 0, 0), "Agency FB", 55)
+        botao_doll = Botao(100, 650, 80, 80, (239, 216, 237),
+                           "Doll", (0, 0, 0), "Agency FB", 55)
 
-        botao_green_book = Botao(430, 350, 200, 80, (239, 216, 237),
-                           "Green Book", (0, 0, 0), "Agency FB", 55)
+        botao_green_book = Botao(100, 550, 80, 80, (239, 216, 237),
+                                 "Green Book", (0, 0, 0), "Agency FB", 55)
 
-        botao_heart_scale = Botao(430, 350, 200, 80, (239, 216, 237),
-                           "Heart Scale", (0, 0, 0), "Agency FB", 55)
+        botao_heart_scale = Botao(100, 450, 80, 80, (239, 216, 237),
+                                  "Heart Scale", (0, 0, 0), "Agency FB", 55)
 
-        botao_letter = Botao(430, 350, 200, 80, (239, 216, 237),
-                           "Letter", (0, 0, 0), "Agency FB", 55)  
+        botao_letter = Botao(100, 350, 80, 80, (239, 216, 237),
+                             "Letter", (0, 0, 0), "Agency FB", 55)
 
-        botao_pearl = Botao(430, 350, 200, 80, (239, 216, 237),
-                           "Pearl", (0, 0, 0), "Agency FB", 55)
+        botao_pearl = Botao(100, 250, 80, 80, (239, 216, 237),
+                            "Pearl", (0, 0, 0), "Agency FB", 55)
 
-        botao_pink_heart = Botao(430, 350, 200, 80, (239, 216, 237),
-                           "", (0, 0, 0), "Agency FB", 55)  
+        botao_pink_heart = Botao(250, 650, 80, 80, (239, 216, 237),
+                                 "", (0, 0, 0), "Agency FB", 55)
 
-        botao_teapot = Botao(430, 350, 200, 80, (239, 216, 237),
-                           "", (0, 0, 0), "Agency FB", 55) 
+        botao_teapot = Botao(250, 550, 80, 80, (239, 216, 237),
+                             "", (0, 0, 0), "Agency FB", 55)
 
-        botao_ticket = Botao(430, 350, 200, 80, (239, 216, 237),        
-                           "", (0, 0, 0), "Agency FB", 55)    
+        botao_ticket = Botao(250, 450, 80, 80, (239, 216, 237),
+                             "", (0, 0, 0), "Agency FB", 55)
 
-        botao_wreathe_of_flowers = Botao(430, 350, 200, 80, (239, 216, 237),
-                           "Wreathe of Flowers", (0, 0, 0), "Agency FB", 55)         
+        botao_wreathe_of_flowers = Botao(250, 350, 80, 80, (239, 216, 237),
+                                         "Wreathe of Flowers", (0, 0, 0), "Agency FB", 55)
 
-       
-        dicionario_botoes = {0: botao_doll, 1: botao_green_book, 2: botao_heart_scale, 
-                             3: botao_letter, 4: botao_pearl, 5: botao_pink_heart , 
+        dicionario_botoes = {0: botao_doll, 1: botao_green_book, 2: botao_heart_scale,
+                             3: botao_letter, 4: botao_pearl, 5: botao_pink_heart,
                              6: botao_teapot, 7: botao_ticket, 8: botao_wreathe_of_flowers}
 
         botao_selecionado = 0
-   
+
      # Loop principal
         running = True
         while running:
@@ -124,7 +133,7 @@ def iniciar(self):
             botao_doll.desenhar_botao(screen)
             botao_green_book.desenhar_botao(screen)
             botao_heart_scale.desenhar_botao(screen)
-            botao_letter.desenhar_botao(screen) 
+            botao_letter.desenhar_botao(screen)
             botao_pearl.desenhar_botao(screen)
             botao_pink_heart.desenhar_botao(screen)
             botao_teapot.desenhar_botao(screen)
@@ -160,7 +169,7 @@ def iniciar(self):
             elif dicionario_botoes[botao_selecionado] == 4:
                 screen.blit(pearl_scale, (100, 100))
                 clock.tick(7)
-            elif   dicionario_botoes[botao_selecionado] == 5:
+            elif dicionario_botoes[botao_selecionado] == 5:
                 screen.blit(pink_heart_scale, (100, 100))
                 clock.tick(7)
             elif dicionario_botoes[botao_selecionado] == 6:
@@ -182,26 +191,19 @@ def iniciar(self):
                 if event.type == pg.QUIT:
                     return ListaRetornos.sair.name
 
-            screen.blit(doll_scale, (800, 475))
-            screen.blit(green_book_scale, (800, 475))
-            screen.blit(heart_scale, (800, 475))
-            screen.blit(letter_scale, (800, 475))
-            screen.blit(pearl_scale, (800, 475))
-            screen.blit(pink_heart_scale, (800, 475))
-            screen.blit(teapot_scale, (800, 475))
-            screen.blit(ticket_scale, (800, 475))
-            screen.blit(wreathe_of_flowers_scale, (800, 475))
-        
+            screen.blit(doll_scale, (100, 650))
+            screen.blit(green_book_scale, (100, 550))
+            screen.blit(heart_scale, (100, 450))
+            screen.blit(letter_scale, (100, 350))
+            screen.blit(pearl_scale, (100, 250))
+            screen.blit(pink_heart_scale, (250, 650))
+            screen.blit(teapot_scale, (250, 550))
+            screen.blit(ticket_scale, (250, 450))
+            screen.blit(wreathe_of_flowers_scale, (250, 350))
+
             pg.display.flip()
             pg.display.update()
             clock.tick(self.fps)
 
-            
-          
-TelaInventario("Inventário", "niveis/personagem/anyaar.png").iniciar()            
 
-
-
-            
-
-           
+TelaInventario("Inventário", "niveis/personagem/anyaar.png").iniciar()
