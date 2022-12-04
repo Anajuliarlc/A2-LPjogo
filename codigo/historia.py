7f33fbb655e49b0dfa61c3f8e9f1e8b0babe5cfc
Tatiana Lage <tatiana.lage28@gmail.com>
2022-12-04 02:23:36 -0300

    Atualize inventario

---
+++ codigo/historia.py
@@ -0,0 +1,11 @@
+class Historia():
+    """História do objeto"""
+    def __init__(self, historia):
+        """
+        :param imagem: Caminho de acesso da imagem do objeto em png
+        :type imagem: str
+        :param historia: Caminho txt com texto informativo da história
+        :type imagem: str
+        :param status: Texto que informa se objeto está liberado ou bloqueado  
+        :type imagem: str 
+        """
